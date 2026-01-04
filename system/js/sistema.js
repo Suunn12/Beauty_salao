df2b50d20408b0a4afbda6259fc50206abaa0a08
Suunn12 <danielmirandagenovez@gmail.com>
2026-01-04 13:25:20 -0300

    Add files via upload

---
+++ system/js/sistema.js
@@ -0,0 +1,33 @@
+//________________ Script ________________
+//________________ Globais ________________
+//________________ Funcoes ________________
+//________________ Estruturas ________________
+
+
+
+//Anima os botoes padroes
+const botao_padrao = document.querySelectorAll(".btn_padrao");
+
+botao_padrao.forEach(botao => {
+	botao.addEventListener("click", () => {
+		botao.classList.add("abrir_botao");
+	});
+	botao.addEventListener("animationend", () => {
+		botao.classList.remove("abrir_botao");
+		
+		if(botao.classList.contains("btn_inicial")){
+			//alert("inicial");
+			window.location.href = "/index.html";
+		}
+		else if(botao.classList.contains("btn_agenda")){
+			//alert("agenda");
+			window.location.href = "/system/html/agenda.html";
+		}
+		else if(botao.classList.contains("btn_resumo")){
+			//alert("resumo");
+			window.location.href = "/system/html/resumo.html";
+		}
+	});
+});
+
+//________________ Chamadas ________________
